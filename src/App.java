68d5ad6b687c462a5cbaedef972442ded25de40f
educole3335 <usr3335@salesianos-lacuesta.net>
2025-06-10 12:49:24 +0100

    feat(EjercicioTres): hice todo el metodo del ejercicio y una pruba y se imprime todo de forma correcta

--- src/App.java
+++ src/App.java
@@ -1,10 +1,13 @@
+import java.io.File;
 import net.salesianos.actividades.dos.EjercicioDos;
+import net.salesianos.actividades.tres.EjercicioTres;
 import net.salesianos.actividades.uno.EjercicioUno;
 
 public class App {
     public static void main(String[] args) throws Exception {
         EjercicioUno.crearYEscribirArchivo();
         EjercicioDos.mostrarContenidoFicheroConDecimal("files/ficherito2.txt", System.out);
+        EjercicioTres.combinarFicheros(new File("files/ficherito.txt"), new File("files/ficherito2.txt"), new File("files/ficherito3.txt"));
     }
   
 }
