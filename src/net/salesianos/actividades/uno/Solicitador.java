999713d659db925a00e6e57f4c4e597731068d5f
educole3335 <usr3335@salesianos-lacuesta.net>
2025-06-10 10:07:53 +0100

    feat(Solicitador): añadir función solicitarTextoConLongitudMinima para pedir texto con longitud mínima

--- src/net/salesianos/actividades/uno/Solicitador.java
+++ src/net/salesianos/actividades/uno/Solicitador.java
@@ -1,6 +1,20 @@
 package net.salesianos.actividades.uno;
+import java.util.Scanner;
 
 public class Solicitador {
 
+        public static String solicitarTextoConLongitudMinima(int longitudMinima) {
+        Scanner sc = new Scanner(System.in);
+        String texto;
+        do {
+            System.out.print("Introduce texto con longitud minima de " + longitudMinima + " caracteres: ");
+            texto = sc.nextLine();
+            if (texto.length() < longitudMinima) {
+                System.out.println("El texto debe tener al menos " + longitudMinima + " caracteres. Te faltan " + (longitudMinima - texto.length()));
+            }
+        } while (texto.length() < longitudMinima);
+        sc.close();
+        return texto;
+    }
 }
 
